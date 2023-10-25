ee8ac8a1f7315cb176bdd724511fd2c8a1276ad1
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2023-10-25 10:03:00 +0300

    removed api key

--- Day3/200_VT_IP_Address_Report.py
+++ Day3/200_VT_IP_Address_Report.py
@@ -4,7 +4,7 @@ url = "https://www.virustotal.com/api/v3/ip_addresses/179.42.225.154"
 
 headers = {
     "accept": "application/json",
-    "x-apikey": "9c0a5015075f6d4d3c9395a870041c65263b5f4c1bfcbfbb91ab19d945f0ba8c"
+    "x-apikey": ""
 }
 
 response = requests.get(url, headers=headers)
